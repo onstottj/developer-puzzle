6f9c4cde36d13186d10c4c6bc9fa1f1b63d6c53c
Jon Onstott <jononstott@gmail.com>
2020-03-19 14:31:36 -0700

    Add sandbox IEX keys

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiKey: '',
+  apiKey: 'Tpk_ec3ded92133a4c63bebf9b0388df6f4e',
   apiURL: 'https://sandbox.iexapis.com'
 };
