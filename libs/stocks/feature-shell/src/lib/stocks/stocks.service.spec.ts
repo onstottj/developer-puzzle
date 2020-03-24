c7f629a8b197067508b54ec478cd33eacfcf0395
Jon Onstott <jononstott@gmail.com>
2020-03-24 13:50:47 -0700

    Clean up some test code

--- libs/stocks/feature-shell/src/lib/stocks/stocks.service.spec.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import moment from 'moment';
 import { PriceQuery } from '@coding-challenge/stocks/data-access-price-query';
+import moment from 'moment';
 import { StocksService } from './stocks.service';
 
 describe('StocksService', () => {
@@ -16,12 +16,10 @@ describe('StocksService', () => {
   });
 
   it('should filter stock prices using From and To dates', function() {
-    const marchFirst = { date: '2015-01-01', close: 150 } as PriceQuery;
-    const marchFifth = { date: '2015-03-05', close: 165 } as PriceQuery;
     const prices: PriceQuery[] = [
       { date: '2010-01-01', close: 150 } as PriceQuery,
-      marchFirst,
-      marchFifth,
+      { date: '2015-01-01', close: 150 } as PriceQuery,
+      { date: '2015-03-05', close: 165 } as PriceQuery,
       { date: '2020-01-01', close: 150 } as PriceQuery
     ];
 
@@ -31,6 +29,9 @@ describe('StocksService', () => {
       moment('2015-12-31')
     );
 
-    expect(filteredPrices).toEqual([marchFirst, marchFifth]);
+    expect(filteredPrices).toEqual([
+      { date: '2015-01-01', close: 150 } as PriceQuery,
+      { date: '2015-03-05', close: 165 } as PriceQuery
+    ]);
   });
 });
