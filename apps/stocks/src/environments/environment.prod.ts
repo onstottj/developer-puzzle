ace9d47e1edc06c37956cf715311399cb2eec5de
Jon Onstott <jononstott@gmail.com>
2020-03-23 21:06:09 -0700

    Do some cleanup

--- apps/stocks/src/environments/environment.prod.ts
+++ apps/stocks/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import { StocksAppConfig } from '@coding-challenge/stocks/data-access-app-config
 
 export const environment: StocksAppConfig = {
   production: true,
-  apiKey: 'Tpk_ec3ded92133a4c63bebf9b0388df6f4e',
+  apiKey: '',
   apiURL: 'https://sandbox.iexapis.com'
 };
