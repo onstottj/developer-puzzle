992fdbb214a43b58c4ac061e696ec576e8d25a8d
Jon Onstott <jononstott@gmail.com>
2020-03-24 21:08:03 -0700

    Add an E2E test

--- apps/stocks-e2e/src/integration/app.spec.ts
+++ apps/stocks-e2e/src/integration/app.spec.ts
@@ -1,9 +1,30 @@
-import { getGreeting } from '../support/app.po';
+import {
+  getGoogleChart,
+  getGreeting,
+  getPeriodInput,
+  getPeriodOption,
+  getPricesList,
+  getSubmitButton,
+  getSymbolInput
+} from '../support/app.po';
 
 describe('Hello Nx', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     getGreeting().contains('Welcome to Stocks!');
+  });
+
+  it('should display a chart and prices list', function() {
+    getSymbolInput().type('AAPL');
+    getPeriodInput().click();
+    getPeriodOption()
+      .contains('Two years')
+      .click();
+
+    getSubmitButton().click();
+
+    getGoogleChart();
+    getPricesList();
   });
 });
